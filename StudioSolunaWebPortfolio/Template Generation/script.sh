c55ee45b81f864fc9ab63d97cb6d7abad2c48ee0
Josh Folsom <josh.folsom@yahoo.com>
2020-10-15 07:35:44 -0700

    unifie project

---
+++ StudioSolunaWebPortfolio/Template Generation/script.sh
@@ -0,0 +1,13 @@
+mkdir abstract pages layout base components 
+cd abstract
+touch _function.scss _mixins.scss _variables.scss
+cd .. 
+cd base 
+touch _animations.scss _typography.scss _utilities.scss _base.scss
+# This is a comment
+# Use below imports in your main inde.scss file
+# @import "./abstract/variables";
+# @import "./layout/header";
+# @import "./base/animations";
+# @import "./base/typography";
+# @import "./components/button";
